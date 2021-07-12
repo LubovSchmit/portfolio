07de276de0800522b01dcbdec59330cebb73cec1
LiubovSchmit <liubou.schmit@gmail.com>
2021-07-13 01:24:52 +0200

    changes

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -8,11 +8,13 @@ function Contacts() {
         <div className={style.contactsBlock}>
             <div className={stylesContainer.container}>
                 <div className={style.contacts}>
+                    <div className={style.contactme}>
                     <h3>Contact me</h3>
+                    </div>
                     <form className={style.form}>
-                        <input value={'Name'} className={style.input}></input>
-                        <input value={'Email'} className={style.input}></input>
-                        <textarea value={'Message'} className={style.textarea}></textarea>
+                        <input value={'Name'} className={style.input}/>
+                        <input value={'Email'} className={style.input}/>
+                        <textarea value={'Message'} className={style.textarea}/>
 
                     </form>
                     <button className={style.buttonContacts}>Send message</button>
--- src/main/Main.js
+++ src/main/Main.js
@@ -6,16 +6,21 @@ import styleContainer from '../commun/styles/Container.module.css'
 function Main() {
     return (
         <div className={style.mainBlock}>
+
             <div className={styleContainer.container}>
-                <div className={style.text}>
-                    <span>Hi there</span>
-                    <h1>I am Lubov Schmit</h1>
-                    <p>I am Frontend Developer</p>
-                    <p>I build web interfaces</p>
+                <div className={style.main}>
+                    <div className={style.text}>
+                        <span>Hi there</span>
+                        <h1>I am Lubov Schmit</h1>
+                        <p>I am Frontend Developer</p>
+                        <p>I build web interfaces</p>
+                    </div>
+
+                    <div className={style.photo}>
+                        <img src={'https://thumbs.dreamstime.com/b/geisha-en-kimono-noir-portrait-d-une-jeune-femme-asiatique-magnifique-black-159431674.jpg'} alt={'profile'}/>
+                    </div>
+
                 </div>
-
-                <div className={style.photo}></div>
-
             </div>
         </div>
     );
@@ -24,21 +29,3 @@ function Main() {
 export default Main;
 
 
-{/* <div className="art-lg-text art-code mb-25">
-                &lt;<i>code</i>&gt;
-                I build
-                <span className="txt-rotate" data-period="2000"
-                      data-rotate="[ &quot;web interfaces.&quot; ]">
-                    <span className="wrap">
-                        web interfaces
-                    </span>
-            </span>
-                &lt;/<i>code</i>&gt;
-            </div>
-            <span className="txt-rotate" data-period="2000"
-                  data-rotate="[ &quot;web interfaces.&quot;,&quot;ios and android applications.&quot;,&quot;design mockups.&quot;,&quot;automation tools.&quot; ]">
-                <span className="wrap">
-                    design m
-                </span>
-            </span>*/
-}
