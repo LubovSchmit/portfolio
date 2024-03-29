3356cecc72c61c42df289cc4ee9cb0b2711381c6
LiubovSchmit <liubou.schmit@gmail.com>
2024-03-29 11:13:43 +0100

    refactor to FSD

--- src/app/ui/App.js
+++ src/app/ui/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.scss';
-import Header from "./header/Header";
-import Main from "./main/Main";
-import Skills from "./skills/Skills";
-import Projects from "./projects/Projects";
-import Hire from "./hire/Hire";
-import Contacts from "./contacts/Contacts";
-import Footer from "./footer/Footer";
+import Header from "../header/Header";
+import Main from "../main/Main";
+import Hire from "../hire/Hire";
+import Skills from "../skills/Skills";
+import Projects from "../projects/Projects";
+import Contacts from "../contacts/Contacts";
+import Footer from "../footer/Footer";
+
 
 function App() {
     return (
